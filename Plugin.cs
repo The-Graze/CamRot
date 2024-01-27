d02ae09cd9a22b8be7c5df206cfd8a7cb2e0fdea
Graze <reece2004f@gmail.com>
2024-01-27 20:48:05 +0000

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,38 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Utilla;
+
+namespace CamRotFix
+{
+    [BepInDependency("org.legoandmars.gorillatag.utilla")]
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        Transform lookat,mainCameraTransform;
+
+        ConfigEntry<bool> face;
+        void Awake()
+        {
+            Events.GameInitialized += OnGameInitialized;
+            face = Config.Bind("Settings", "Direction", false);
+        }
+
+        void OnGameInitialized(object sender, EventArgs e)
+        {
+            mainCameraTransform = Camera.main.transform;
+            lookat = mainCameraTransform.Find("Camera Follower");
+        }
+
+        void Update()
+        {
+            lookat.localRotation = Quaternion.Euler(face.Value ? new Vector3(0, 180, 0) : Vector3.zero);
+            if (Keyboard.current.slashKey.wasPressedThisFrame)
+            {
+                face.Value = !face.Value;
+            }
+        }
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace CamRotFix
+{
+    internal class PluginInfo
+    {
+        public const string GUID = "com.graze.gorillatag.camrot";
+        public const string Name = "CamRot";
+        public const string Version = "1.0.0";
+    }
+}
